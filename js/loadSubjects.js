34c2e43b53b8e46d7b4b2a7040d773bf8b373d9e
rhofp <rhodfra@gmail.com>
2019-07-12 00:39:49 -0500

    using dollar notation for html strings

--- js/loadSubjects.js
+++ js/loadSubjects.js
@@ -10,20 +10,25 @@ const loadSubjects = ()=>{
             <div class="card-body">
             <div class="row">
                 <div class="col-md-8">
-                    <p class="card-text"> <b>Gpo.`;
-            output += grupo.gpo+ `</b> `+ grupo.profesor+ `</p>
+                    <p class="card-text"> <b>Gpo.
+                    ${grupo.gpo} </b>  ${grupo.profesor} </p>
                 </div>
                 <div class="col-md-4 text-right">
-                <button class="btn btn-primary mb-2" type="button" onclick="searchCell(`+keysub+`,`+grupo.gpo+`)" id="">Agregar</button>
+                <button class="btn btn-primary mb-2" type="button" 
+                    onclick="searchCell(${keysub},${grupo.gpo})" 
+                    id="">
+                    Agregar
+                </button>
                 </div>
             </div>
-            <p class="card-text"><b>`;
-            output +=`Horario:</b> ` + grupo.horario_ini +` a ` +grupo.horario_fin + `&nbsp; <b>Días: </b>` + p_days(grupo.dias) +`</p></div></div>`;
+            <p class="card-text"><b>
+            Horario:</b> ${grupo.horario_ini}  a ${grupo.horario_fin} &nbsp; 
+            <b>Días: </b> ${p_days(grupo.dias)} </p></div></div>`;
         }  
     }else{
         output += `
-        <div class="alert alert-warning" role="alert">`
-            + keysub + ` is not in our database yet or is not a valid key </div>`;
+        <div class="alert alert-warning" role="alert">
+            ${keysub} is not in our database yet or is not a valid key </div>`;
     }
     document.getElementById("list-subjects").innerHTML = output;  
 }
