d9ea678ba316908a8763cadb1559957c0ca7034b
rhofp <rhodfra@gmail.com>
2019-07-13 03:07:14 -0500

    movi de lugar funciones js de horario

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,96 +4,106 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Horario FI</title>
+
+        <!-- Styles -->
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+        <link href="{{ asset('css/horario.css') }}" rel="stylesheet">
+
+        <!-- Scripts -->
+        <script src="{{ asset('js/app.js') }}" defer></script>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
     </head>
     <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+        <nav class="navbar navbar-expand-lg navbar-light bg-white shadow-sm">
+            <div class="container">
+                <a class="navbar-brand" href="{{ url('/')}}">Horario FI</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" 
+                    data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="https://github.com/rhofp/horario-fi">Fork me</a>
+                        </li>
+                    </ul>
                 </div>
-            @endif
+            </div>    
+        </nav>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
-                </div>
+        <!-- Page Content -->
+        <div class="container mt-3">
+            <div class="row">
+                <div class="col-sm-12">
+                    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+                    <li class="nav-item">
+                        <a class="nav-link active" id="pills-schedule-table-tab" data-toggle="pill" 
+                            href="#pills-schedule-table" role="tab" aria-controls="pills-schedule-table" 
+                            aria-selected="false"
+                            >Mi horario
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="pills-search-tab" data-toggle="pill" href="#pills-search" 
+                            role="tab" aria-controls="pills-search" aria-selected="true">
+                            Buscar materias
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" id="pills-group-add-tab" data-toggle="pill" href="#pills-group-add"
+                            role="tab" aria-controls="pills-group-add" aria-selected="false">
+                            Agregar mis materias
+                        </a>
+                    </li>
+                    </ul>
+                    
+                    
+                    <div class="tab-content" id="pills-tabContent">
+                    <div class="tab-pane fade" id="pills-search" role="tabpanel" aria-labelledby="pills-search-tab">
+                        <form class="form-inline">
+                            <input class="form-control mb-2 mr-sm-2" placeholder="Clave asignatura" id="subjectkey">
+                            <button class="btn btn-primary mb-2 " type="button" onclick="loadSubjects()">Buscar</button>
+                        </form>
+                        <div id="list-subjects"> </div>  
+                    </div>
+                    <div class="tab-pane fade" id="pills-group-add" role="tabpanel" aria-labelledby="pills-group-add-tab">
+                        <form class="form-inline">
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                        <input type="text" class="form-control mb-2 mr-sm-2" placeholder="Clave">
+                        <input type="text" class="form-control mb-2 mr-sm-2" placeholder="Grupo">
+                        
+                        <button type="button" class="btn btn-primary mb-2">Agregar</button>
+                        </form>              
+                        <div id="list-subjects-added"> </div>   
+                    </div>
+                    <div class="tab-pane fade show active" id="pills-schedule-table" role="tabpanel" 
+                        aria-labelledby="pills-schedule-table-tab">
+                        <div class="col-sm-12 col-md-12 table-responsive" id="schedule-table">
+                    </div>
+                    </div>
+                    </div> 
                 </div>
             </div>
         </div>
+        <!-- /.container -->
+
+        <!-- Footer -->
+        <footer class="py-5 bg-dark">
+        <div class="container">
+        <p class="m-0 text-center text-white">Open source project. Feel free to contribute. </p>
+        </div>
+        <!-- /.container -->
+        </footer>
+        <script src="{{ asset('js/TimeClass.js') }}"></script>
+        <script src="{{ asset('js/subjectsData.js') }}"></script>
+        <script src="{{ asset('js/loadTable.js') }}"></script>
+        <script src="{{ asset('js/loadSubjects.js') }}"></script>
+        <script src="{{ asset('js/searchCell.js') }}"></script>
     </body>
 </html>
