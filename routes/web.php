f4b8d614180211053c1e43ab7cb96a2979ad2aad
rhofp <rhodfra@gmailc.com>
2019-07-17 19:36:33 -0500

    modifico router para integrar by k&g

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,4 @@
 <?php
-use Illuminate\Support\Facades\Input;
-use App\Subject;
-use App\Group;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -21,40 +18,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::post ( '/key', function () {
-	$q = Input::get ( 'subjectkey' );
-	if($q != ""){
-		$s = App\Subject::find($q);
-		if ( $s ){ 
-			$groups = $s->groups()->get();
-			/*
-				With more practise this should work
-				$groups = App\Subject::where('key',1644)->with('groups')->get()->all();
-			*/
-			if (count ( $groups ) > 0)
-				return view ( 'welcome' )->with('subject',$s->name)->withDetails ( $groups )->withQuery ( $q);
-			else
-				return view ( 'welcome' )->withMessage ( 'No hay grupos para esta clave' );
-		}else				
-			return view ( 'welcome' )->withMessage ( 'La clave es incorrecta o no esta en nuestra base de datos, aún.' );		
-	}
-	return view ( 'welcome' )->withMessage ( 'No Details found. Try to search again !' );
-} );
+Route::get ( '/addByKey', 'ScheduleController@addByKey')->name('addByKey');
 
-
-Route::post ( '/key_sub', function () {
-	$q = Input::get ( 'key' );
-	$g = Input::get ( 'group' );
-
-	if($q != ""){
-		$s = App\Subject::find($q);
-		if ( $s ){ 
-			$group = $s->groups()->get()->where('gpo',$g);
-			if (count ( $group ) > 0)
-				return view ( 'welcome' )->with('gpo',$group)->withQuery ( $q);
-		}
-		return view ( 'welcome' )->withMessage ( 'Clave o grupo no encontrado.' );
-
-	}
-	return view ( 'welcome' )->withMessage ( 'No Details found. Try to search again !' );
-} );
+Route::get ( '/addByKeyAndSub', 'ScheduleController@addByKeyAndSub')->name('addByKeyAndSub');
