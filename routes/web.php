bfe85f4adda45ff5bbdadcb248a8ecc36b6ab48d
rhofp <rhodfra@gmail.com>
2019-07-20 00:37:13 -0500

    Remuevo automatic-seeder

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,7 @@ Route::get('/', function () {
     return view('welcome')->with('subjects',Session::get('subjects'));
 });
 
-Auth::routes();
-Route::get('/home', 'HomeController@index')->name('home');
-
 Route::get('/update', 'ScheduleController@update')->name('update');
-
 
 Route::get('/flush', function () {
     //reemplazar esto por la instruccion que solo elimna la v. de s. llamada subjects
@@ -26,45 +22,5 @@ Route::get('/flush', function () {
     return view('welcome');
 });
 
-Route::get('/seed', function () {
-
-    Session::flush();
-
-    $s1 = [ 
-        "key"=>434,
-        "name" => "COMPILADORES",
-        "gpo" => 1,
-        "profesor" => "ING.ADRIAN ULISES MERCADO MARTINEZ",
-        "horario" => "13:00-15:00",
-        "dias" => "Lun, Mie"
-    ];
-
-    Session::push('subjects', $s1);
-
-    $s2 =  [
-        "key"=>1644,
-        "name" => "BASE DE DATOS",
-        "gpo" => 3,
-        "profesor" => "ING. JORGE ALBERTO RODRIGUEZ CAMPOS",
-        "horario" => "7:00-9:00",
-        "dias" => "Mar, Jue, Vie"
-    ];
-
-    Session::push('subjects', $s2);
-
-
-    $s3 =  [
-        "key"=>406,
-        "name" => "INTELIGENCIA ARTIFICIAL",
-        "gpo" => 2,
-        "profesor" => "ING. JORGE ALBERTO HERNANDEZ NIETO",
-        "horario" => "15:00-17:00",
-        "dias" => "Lun, Mie"
-    ];
-
-    Session::push('subjects', $s3);    
-    
-    return view('welcome');
-
-});
-
+Auth::routes();
+Route::get('/home', 'HomeController@index')->name('home');
