f9bc6a688412b48bdb0e617e8d99e1f68d77ef09
Rodrigo <rhodfra@gmail.com>
2020-10-22 16:58:54 -0500

    Makeing modules of header and left times

---
+++ src/components/Timetable.vue
@@ -0,0 +1,117 @@
+<template>
+  <div class="timetable-grid-display">
+    <div class="schedule-header">{{ leftTitle }}</div>
+    <!-- Start of timetable header -->
+    <div
+      v-for="(day, i, index) in days"
+      :key="i"
+      :style="daysHeaderGridPosStyle(index)"
+      class="days-header"
+    >
+      {{ day }}
+    </div>
+    <!-- End of timetable header -->
+    <!-- Start of left times -->
+    <div
+      v-for="n in lapsus + 1"
+      :key="n"
+      :style="leftTimePosStyle(n)"
+      class="left-schedule-time"
+    >
+      {{ numberToTime(n) }}
+    </div>
+    <!-- End of left times -->
+  </div>
+</template>
+
+<script>
+import daysEnum from '../common/days';
+import Time from '../common/time';
+import { sumTimes, numToTime } from '../common/scheduler';
+
+export default {
+  data: () => ({
+    /**
+     * daysEmum asigned to days let us retrive the enum and load it as
+     * a VueJS reactive property
+     */
+    days: daysEnum,
+    leftTitle: 'Horario',
+    /**
+     * The following 3 variables help us to decide from which hour render
+     * the timetable
+     * lapsus is the number of iteration
+     * By default, iteration are each 15 minutes
+     */
+    startTime: new Time(7, 0),
+    endTime: new Time(22, 0),
+    lapsus: 60,
+  }),
+  methods: {
+    /**
+     * Method to return styles that will be injected in a HTML component to
+     * position it on the grid
+     * @param index, the position of HTML component
+     * @return js-css string
+     */
+    daysHeaderGridPosStyle(index) {
+      const startCol = index + 2; // +2 por el header
+      const endCol = startCol + 1;
+      return {
+        'grid-row': '1/2',
+        'grid-column': `${startCol}/${endCol}`,
+      };
+    },
+    /**
+     * This methos allow us to calculate the string of column and row grid
+     * to apply fo each time
+     * @param index Is the index vertical movement while rendering the times
+     */
+    leftTimePosStyle(index) {
+      const startRow = index + 1; // +1 por el header
+      const endRow = startRow + 1;
+      return {
+        'grid-column': '1/2',
+        'grid-row': `${startRow}/${endRow}`,
+      };
+    },
+    /**
+     * This function is a wrapper in order not to use time operations directly
+     * from Build-by-me time library
+     */
+    numberToTime(n) {
+      // TODO:- Explain how is n?
+      return sumTimes(this.startTime, numToTime(n - 1)).print();
+    },
+  },
+};
+</script>
+
+<style scoped>
+/* having display:grid makes possible the timetable */
+.timetable-grid-display {
+  display: grid;
+  grid-template-columns: 0.5fr repeat(6, 1fr);
+  font-size: 12px;
+  text-align: center;
+  grid-gap: 1.5px;
+}
+
+.schedule-header {
+  grid-column: 1/2;
+  grid-row: 1/2;
+  border-radius: 6px;
+  font-size: 14px;
+  border: 1.5px solid blue;
+}
+
+.days-header {
+  border-radius: 6px;
+  font-size: 14px;
+  border: 1.5px solid blue;
+}
+
+.left-schedule-time {
+  border-top: 1.5px solid blue;
+}
+</style>
---
+++ src/views/Schedule.vue
@@ -0,0 +1,36 @@
+<template>
+  <!-- This template will be home users app -->
+  <v-container fluid>
+    <!-- Floating button -->
+    <v-btn
+      absolute
+      fixed
+      dark
+      fab
+      bottom
+      right
+      color="blue"
+      class="mb-10 d-print-none"
+      @click="openPanelDialog = true"
+    >
+      <v-icon>mdi-plus</v-icon>
+    </v-btn>
+    <v-row>
+      <v-col md="12" sm="12" class="pt-0"> <Timetable /> </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+import Timetable from '../components/Timetable.vue';
+
+export default {
+  // Schedule -> Home refactor will be perform!
+  name: 'Shedule',
+  components: { Timetable },
+  data: () => ({
+    // TODO:- Make dialog work
+    openPanelDialog: false,
+  }),
+};
+</script>
