8ce8bd210e2508182c2216ebc2e2f677f875bb13
Rodrigo <rhodfra@gmail.com>
2021-04-21 23:35:47 -0500

    Updated deployment script to have diferent commit messages; also added new path for github pages repo

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
+#!/bin/sh
+# Setup variables
+localRepoPath="/f/dev/roverty.github.io/"
+commitMessage=${*:-"my schedule deployment"}
+
 npm run lint --fix
 npm run build
-cp -rf dist/* README.md /f/dev/rhofp.github.io/
-cd /f/dev/rhofp.github.io/
+cp -rf dist/* README.adoc assets/ docs/ ${localRepoPath}
+cd ${localRepoPath}
 git add -A
-git commit -m "my schedule deployment"
-git push -u origin master
+git commit -m "${commitMessage}"
+git push -u origin main
 cd -
